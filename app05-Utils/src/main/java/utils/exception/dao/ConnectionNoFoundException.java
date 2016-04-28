3b9d0930521a9859fbcf4c9b5ab53f5044826b74
Gabriel <sanabriaGabriel@gmail.com>
2016-04-27 19:19:56 -0600

    Mover proyecto Utils

--- app05-Utils/src/main/java/utils/exception/dao/ConnectionNoFoundException.java
+++ app05-Utils/src/main/java/utils/exception/dao/ConnectionNoFoundException.java
@@ -1,7 +1,8 @@
 package utils.exception.dao;
 
 /**
- * Created by Gabriel on 13/04/2016.
+ * Created by Gabriel
+ * on 13/04/2016.
  */
 public class ConnectionNoFoundException extends RuntimeException {
     public ConnectionNoFoundException(){
--- app05-Utils/src/main/java/utils/exception/dao/DaoOperationException.java
+++ app05-Utils/src/main/java/utils/exception/dao/DaoOperationException.java
@@ -1,7 +1,8 @@
 package utils.exception.dao;
 
 /**
- * Created by Gabriel on 13/04/2016.
+ * Created by Gabriel
+ * on 13/04/2016.
  */
 public class DaoOperationException extends Exception {
     public DaoOperationException(String msj){
