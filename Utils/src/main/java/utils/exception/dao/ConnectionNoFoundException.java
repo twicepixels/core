e5537fa6956a72880a1669c603a951625552ade3
Gabriel <sanabriaGabriel@gmail.com>
2016-04-27 18:25:24 -0600

    AWSDao para la conexión a la base de datos de DynamoDb

---
+++ Utils/src/main/java/utils/exception/dao/ConnectionNoFoundException.java
@@ -0,0 +1,10 @@
+package utils.exception.dao;
+
+/**
+ * Created by Gabriel on 13/04/2016.
+ */
+public class ConnectionNoFoundException extends RuntimeException {
+    public ConnectionNoFoundException(){
+        super("conection.error");
+    }
+}
---
+++ Utils/src/main/java/utils/exception/dao/DaoOperationException.java
@@ -0,0 +1,10 @@
+package utils.exception.dao;
+
+/**
+ * Created by Gabriel on 13/04/2016.
+ */
+public class DaoOperationException extends Exception {
+    public DaoOperationException(String msj){
+        super(msj);
+    }
+}
